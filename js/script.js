e2049154396eb683a3b891e14741b28a3aeb3977
ramiro reyna <ramirololreyna@gmail.com>
2024-05-07 13:25:01 -0300

    realizado primer ejercicio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("hola")
+alert("bienvenido!")
+let nombre = prompt("escribe tu nombre");
+let edad = prompt("escribe tu edad");
+
+console.log("tu nombre es " + nombre + " y tienes " + edad + " años" );
+let añoNacimiento = 2024 - edad;
+if (edad < 18) {
+    alert("eres menor de edad")
+} else {
+    alert("eres mayor de edad")
+}
